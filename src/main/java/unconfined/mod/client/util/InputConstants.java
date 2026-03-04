ed128a058f73aed67d7851a6783c83f85763c42d
Taskeren <r0yalist@outlook.com>
2026-03-04 20:06:16 +0800

    Clean up

--- src/main/java/unconfined/mod/client/util/InputConstants.java
+++ src/main/java/unconfined/mod/client/util/InputConstants.java
@@ -14,6 +14,7 @@ import java.lang.annotation.Target;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
+@SuppressWarnings("unused")
 public class InputConstants {
     public static final int KEY_0 = 48;
     public static final int KEY_1 = 49;
